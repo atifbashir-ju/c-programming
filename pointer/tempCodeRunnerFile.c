f3935598414f3268733fda92dfe1f0cb9f1dd844
atifbashir-ju <atifparay16@gmail.com>
2025-11-16 12:19:02 +0530

    pointer

---
+++ pointer/tempCodeRunnerFile.c
@@ -0,0 +1,9 @@
+*ptr +=5;
+//     printf(" x = %d\n, x");
+//     printf(" *ptr = %d\n", *ptr);
+
+//     (*ptr)++;
+//     printf(" x = %d\n", x);
+//     printf(" *ptr = %d\n", *ptr);
+//     return 0;
+// }
