6bcb6695dae31a3f609f2b02da9aabe0b27c02c5
atifbashir-ju <atifparay16@gmail.com>
2025-11-17 19:33:00 +0530

    check prime numebr using function

---
+++ prime-byfunction.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+// function to check prime
+int isPrime(int n) {
+    if(n <= 1)
+        return 0;
+
+    for(int i = 2; i*i <= n; i++) {
+        if(n % i == 0)
+            return 0;   // not prime
+    }
+
+    return 1;   // prime
+}
+
+int main() {
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
